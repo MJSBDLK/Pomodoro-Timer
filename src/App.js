bcd4da16355c3872404670d100728d24e03c818c
MJSBDLK <rqdavismail@gmail.com>
2021-07-23 08:09:49 -0700

    git init commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Pomodoro from "./pomodoro/Pomodoro";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header container">
+        <h1>Pomodoro Timer</h1>
       </header>
+      <div className="container">
+        <Pomodoro />
+      </div>
     </div>
   );
 }
